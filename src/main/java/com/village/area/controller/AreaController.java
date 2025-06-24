f046331dcf7580423954b49918434cf02319713e
Shanmukha Kadumuru <shanmukhak709@gmail.com>
2025-06-24 17:12:44 +0530

    Added frontend path

--- src/main/java/com/village/area/controller/AreaController.java
+++ src/main/java/com/village/area/controller/AreaController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {
+        "https://our-circle-ui.onrender.com",
+        "http://localhost:3000"
+})
 @RestController
 @RequestMapping("/areas")
 public class AreaController {
